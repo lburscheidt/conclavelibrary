2e8f5b1fb60995c32b032628c5ba66b498cebc34
lburscheidt <burscheidt@zohomail.eu>
2025-07-22 22:06:08 +0200

    Quartz sync: Jul 22, 2025, 10:06 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,8 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    /*pageTitle: "Quartz 4",*/
+    pageTitle = "",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
